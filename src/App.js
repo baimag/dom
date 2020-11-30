f47b45f2f42ef8c839736eeec9e2cf3e73f0defe
baimag <baimag02@yandex.ru>
2020-11-30 16:29:51 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {loadTodos} from "./action";
+import React, {useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const todos = useSelector(state => state.todos);
+  const loading = useSelector(state => state.loading);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(loadTodos())
+  },[]);
+
+  return(
+      <div className={"main"}>
+        Список дел : {loading ? "Идет загрузка дел" : (todos.map(todo => {
+        return(
+            <div className={"content"}>
+              {todo.title}
+            </div>
+        )
+      }))}
+
+      </div>
+  )
+}
 export default App;
---
+++ src/action.js
@@ -0,0 +1,12 @@
+export const loadTodos = () => {
+    return function (dispatch){
+        dispatch({type: "start"})
+        fetch("https://jsonplaceholder.typicode.com/posts").then((response)=>
+            response.json()).then(json =>{
+            dispatch({
+                type: "load",
+                payload: json
+            })
+        })
+    }
+}
